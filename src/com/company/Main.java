dbf85bbcc883ddb51235282ba4d85947aec074d7
GANESH GHOSH <ganeshghosh02015@gmail.com>
2019-09-09 18:14:24 +0530

    basic operation

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        int firstnum=34;
+        int secondnum=3;
+        double fnum=34;
+        double snum=3;
+        System.out.println("the sum is:"+(firstnum + secondnum));
+        System.out.println("the subtraction is:"+(firstnum - secondnum));
+        System.out.println("the multiplication is:"+(firstnum * secondnum));
+        System.out.println("the division is:"+(fnum / snum));
+        System.out.println("the remainder is:"+(fnum % snum));
+    }
+}
